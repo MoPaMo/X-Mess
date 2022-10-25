b93de74a830c9c7ae9bb08aa07a801487b8b130c
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2022-10-25 17:55:29 +0200

    create character controller for example cube

---
+++ Assets/Scripts/controller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+           private CharacterController character;
+            public int playerSpeed = 69;
+    // Start is called before the first frame update
+    void Start()
+    {
+           character = GetComponent(typeof(CharacterController)) as CharacterController;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //set direction
+        character.SimpleMove(direction*playerSpeed);
+    }
+}
