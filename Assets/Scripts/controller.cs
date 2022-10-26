cd47699dfb4748b24ccb32101b438124fd91bf4d
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2022-10-26 09:39:37 +0200

    move camera and snow seperately
    it won't rotate even if the character does, thus keeping its relative position of y=8, z=-8 to the character

--- Assets/Scripts/controller.cs
+++ Assets/Scripts/controller.cs
@@ -6,6 +6,7 @@ public class controller : MonoBehaviour
 {
            private CharacterController character;
             public int playerSpeed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +19,12 @@ public class controller : MonoBehaviour
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //set direction
         direction=direction.normalized;
         character.SimpleMove(direction*playerSpeed);
+                     var cam = GameObject.Find("Main Camera");
+
+        Vector3 charPos=character.transform.position; //current character postiioin
+        charPos.y+=8; //correct postition to be diagonally above
+        charPos.z-=8;
+        cam.transform.position=charPos;
+
     }
 }
