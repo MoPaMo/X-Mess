3d49c38e99f759fb0f3ec218c95e03d4c97e6b62
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2022-10-26 09:55:19 +0200

    format code

--- Assets/Scripts/controller.cs
+++ Assets/Scripts/controller.cs
@@ -2,29 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class controller : MonoBehaviour
-{
-           private CharacterController character;
-            public int playerSpeed = 10;
+public class controller: MonoBehaviour {
+  private CharacterController character;
+  public int playerSpeed = 10;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-           character = GetComponent(typeof(CharacterController)) as CharacterController;
+  // Start is called before the first frame update
+  void Start() {
+    character = GetComponent(typeof(CharacterController)) as CharacterController;
 
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //set direction
-        direction=direction.normalized;
-        character.SimpleMove(direction*playerSpeed);
-                     var cam = GameObject.Find("Main Camera");
+  }
+  // Update is called once per frame
+  void Update() {
+    Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //set direction
+    direction = direction.normalized;
+    character.SimpleMove(direction * playerSpeed);
+    var cam = GameObject.Find("Main Camera");
 
-        Vector3 charPos=character.transform.position; //current character postiioin
-        charPos.y+=8; //correct postition to be diagonally above
-        charPos.z-=8;
-        cam.transform.position=charPos;
+    Vector3 charPos = character.transform.position; //current character postiioin
+    charPos.y += 8; //correct postition to be diagonally above
+    charPos.z -= 8;
+    cam.transform.position = charPos;
 
-    }
+  }
 }
