34d3b421f063859691567d724541f72d4285a739
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2022-11-08 11:41:14 +0100

    format code

--- Assets/Scripts/controller.cs
+++ Assets/Scripts/controller.cs
@@ -2,50 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class controller: MonoBehaviour {
-  public CharacterController character;
-  public int playerSpeed = 10;
-  private Vector3 direction = Vector3.zero;
-  private Vector3 lookDirection = Vector3.zero;
-  public float rotTime = 5;
+public class controller: MonoBehaviour
+{
+	public CharacterController character;
+	public int playerSpeed = 10;
+	private Vector3 direction = Vector3.zero;
+	private Vector3 lookDirection = Vector3.zero;
+	public float rotTime = 5;
 
+	// Start is called before the first frame update
+	void Start() {}
 
-  // Start is called before the first frame update
-  void Start() {
-  }
-  
-  // Update is called once per frame
-  void Update() {    
-    direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //set direction
-    direction = direction.normalized;
-    
-    character.SimpleMove(direction * playerSpeed);
-    var cam = GameObject.Find("Main Camera");
-//define look direction, that's not set to 0 if the player is not moving
-    if (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) != Vector3.zero) {
-      lookDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //set direction
-    }
-    if (lookDirection != Vector3.zero) {
-      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), Time.deltaTime * rotTime);
-    }
-    
+	// Update is called once per frame
+	void Update()
+	{
+		direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));	//set direction
+		direction = direction.normalized;
 
-    /*
-     var rot = Quaternion.LookRotation(direction);
-    rot = Quaternion.Slerp(cam.transform.rotation, rot, Time.deltaTime * rotTime);
-    rot = Quaternion.Euler(0, rot.eulerAngles.y, 0);
-    transform.rotation = rot;
+		character.SimpleMove(direction *playerSpeed);
+		var cam = GameObject.Find("Main Camera");
+		//define look direction, that's not set to 0 if the player is not moving
+		if (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) != Vector3.zero)
+		{
+			lookDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));	//set direction
+		}
 
-        if (direction!=Vector3.zero) {
-      Quaternion rotation = Quaternion.LookRotation(direction);
-      transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotTime);
-    }*/
+		if (lookDirection != Vector3.zero)
+		{
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), Time.deltaTime *rotTime);
+		}
 
-    
-    Vector3 charPos = character.transform.position; //current character postiioin
-    charPos.y += 8; //correct postition to be diagonally above
-    charPos.z -= 8;
-    cam.transform.position = charPos;
+		/*
+		 var rot = Quaternion.LookRotation(direction);
+		rot = Quaternion.Slerp(cam.transform.rotation, rot, Time.deltaTime *rotTime);
+		rot = Quaternion.Euler(0, rot.eulerAngles.y, 0);
+		transform.rotation = rot;
 
-  }
+		    if (direction!=Vector3.zero) {
+		  Quaternion rotation = Quaternion.LookRotation(direction);
+		  transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime *rotTime);
+		}*/
+
+		Vector3 charPos = character.transform.position;	//current character postiioin
+		charPos.y += 8;	//correct postition to be diagonally above
+		charPos.z -= 8;
+		cam.transform.position = charPos;
+	}
 }
