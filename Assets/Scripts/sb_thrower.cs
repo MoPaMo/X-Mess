8d8a0b3d185aa7b2974b777fcac337a59690cf94
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2022-12-17 12:09:50 +0100

    short relaod time for snowballs

--- Assets/Scripts/sb_thrower.cs
+++ Assets/Scripts/sb_thrower.cs
@@ -6,18 +6,28 @@ public class sb_thrower : MonoBehaviour
 {
     public float angle = 45f;
     public float force = 1000f;
-    // Start is called before the first frame update
+    private int snowballs = 3;
+    public int maxSnowballs = 3;
+  
     public GameObject projectile;
+   
     public void Throw(Vector3 direction)
     {
-        FindObjectOfType<AudioManager>().Play("Snowball Throw");
-        Vector3 pos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-        GameObject newProjectile = Instantiate(projectile, pos, Quaternion.identity);
-        direction = direction * force;
-        direction = new Vector3(direction.x, Mathf.Sqrt(Mathf.Pow(direction.x, 2f) + Mathf.Pow(direction.z, 2f)) * Mathf.Tan(angle), direction.z);
+        if (snowballs > 0)
+        {
+            snowballs--;
+            FindObjectOfType<AudioManager>().Play("Snowball Throw");
+            Vector3 pos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+            GameObject newProjectile = Instantiate(projectile, pos, Quaternion.identity);
+            direction = direction * force;
+            direction = new Vector3(direction.x, Mathf.Sqrt(Mathf.Pow(direction.x, 2f) + Mathf.Pow(direction.z, 2f)) * Mathf.Tan(angle), direction.z);
 
-        newProjectile.GetComponent<Rigidbody>().AddForce(direction);
-        Debug.Log("Throwing");
+            newProjectile.GetComponent<Rigidbody>().AddForce(direction);
+            Debug.Log("Throwing");
+        }
+        else{
+            snowballs=maxSnowballs;
+        }
     }
 
 
