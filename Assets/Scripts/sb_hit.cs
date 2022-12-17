47b59cdfcdedaa02456bd1ea6e7e7c845956fc7b
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2022-12-17 12:15:37 +0100

    destroy all snowballs

--- Assets/Scripts/sb_hit.cs
+++ Assets/Scripts/sb_hit.cs
@@ -6,12 +6,17 @@ public class sb_hit : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.GetComponents<sb_hitreceiver>().Length != 0)
+        if (!collision.transform.CompareTag("Player"))
         {
-            sb_hitreceiver hitreceiver = collision.transform.GetComponent<sb_hitreceiver>();
-            if (hitreceiver.is_enabled)
-            {
-                hitreceiver.Hit();
+            if (collision.transform.GetComponents<sb_hitreceiver>().Length != 0){
+
+                sb_hitreceiver hitreceiver = collision.transform.GetComponent<sb_hitreceiver>();
+                if (hitreceiver.is_enabled)
+                {
+                    hitreceiver.Hit();
+                    Object.Destroy(this.gameObject);
+                }
+            }else{
                 Object.Destroy(this.gameObject);
             }
         }
