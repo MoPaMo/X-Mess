007c19ee0582c1391745bc4ed4d649e0259b9f5f
MoPaMo <67760881+MoPaMo@users.noreply.github.com>
2022-12-17 08:55:07 +0100

    define dialogueode to disable walking etc

--- Assets/Scripts/controller.cs
+++ Assets/Scripts/controller.cs
@@ -12,13 +12,17 @@ public class controller : MonoBehaviour
     public bool isInHouse = false;
     public bool hasSetHint = false;
     private bool isInteractable = false;
+
+    private bool dialogueMode = false;
+
+
+    //interactable objects
     private GameObject interactableObj = null;
-
-
     private GameObject cam;
     private GameObject OpenHint;
     private OpenHintScript OpenHintText;
     private Collider doorCollider = null;
+
 
     //scripts
     public sb_thrower thrower;
@@ -77,80 +81,82 @@ public class controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //set direction
-        direction = direction.normalized;
+        if(dDialogueMode){
+            direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //set direction
+            direction = direction.normalized;
 
-        if (direction != Vector3.zero && FindObjectOfType<AudioManager>().getSound("Footstep Snow").source.isPlaying == false && FindObjectOfType<AudioManager>().getSound("Footstep House").source.isPlaying == false)
-        {
-            if (isInHouse)
-                FindObjectOfType<AudioManager>().Play("Footstep House");
-            else
-                FindObjectOfType<AudioManager>().Play("Footstep Snow");
-        }
-        //define look direction, that's not set to 0 if the player is not moving
-        if (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) != Vector3.zero)
-        {
-            lookDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //set direction
-        }
-
-        if (lookDirection != Vector3.zero)
-        {
-            // if the difference between the current rotation and the look direction is greater than 0.1, rotate the player
-            if (Vector3.Angle(transform.forward, lookDirection) > 1)
+            if (direction != Vector3.zero && FindObjectOfType<AudioManager>().getSound("Footstep Snow").source.isPlaying == false && FindObjectOfType<AudioManager>().getSound("Footstep House").source.isPlaying == false)
             {
-                //rotate the player
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), rotTime * Time.deltaTime);
+                if (isInHouse)
+                    FindObjectOfType<AudioManager>().Play("Footstep House");
+                else
+                    FindObjectOfType<AudioManager>().Play("Footstep Snow");
             }
-            //set to current rotation if difference is less than 1 but exists
-            else if (transform.rotation != Quaternion.LookRotation(lookDirection))
-            {
-                //rotate the player
-                transform.rotation = Quaternion.LookRotation(lookDirection);
-            }
-
-        }
-
-        Vector3 charPos = character.transform.position; //current character postiioin
-        charPos.y += 8; //correct postition to be diagonally above
-        charPos.z -= 8;
-        if (cam.transform.position != charPos)
-        {
-
-            cam.transform.position = charPos;
-
-            /*Ray r = new Ray(cam.transform.position, cam.transform.forward);
-            RaycastHit hit;
-            if (Physics.Raycast(r, out hit))
-            {
-                if (hit.collider.gameObject != gameObject)
-                {
-                    hit.collider.gameObject.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0.5f);
-                }
-            }*/
-        }
-        if (Input.GetKeyDown(KeyCode.F) && isInteractable)
-        {
-            Debug.Log(interactableObj.GetComponent<DialogueProperties>().dialog);
-        }
-        if (Input.GetKeyDown(KeyCode.F) && hasSetHint && doorCollider != null)
-        {
-            //fade in
-            uis.fadeOut();
-
-            //get the door properties
-            DoorProperties doorProperties = doorCollider.gameObject.GetComponent<DoorProperties>();
-            transform.position = doorProperties.teleportPosition;
-            FindObjectOfType<AudioManager>().Play("Door Enter");
-
-            //set is inside house
-            isInHouse = doorProperties.leadingInside;
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
+            //define look direction, that's not set to 0 if the player is not moving
             if (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) != Vector3.zero)
-                thrower.Throw(direction);
-            else
-                thrower.Throw(transform.forward);
+            {
+                lookDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //set direction
+            }
+
+            if (lookDirection != Vector3.zero)
+            {
+                // if the difference between the current rotation and the look direction is greater than 0.1, rotate the player
+                if (Vector3.Angle(transform.forward, lookDirection) > 1)
+                {
+                    //rotate the player
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), rotTime * Time.deltaTime);
+                }
+                //set to current rotation if difference is less than 1 but exists
+                else if (transform.rotation != Quaternion.LookRotation(lookDirection))
+                {
+                    //rotate the player
+                    transform.rotation = Quaternion.LookRotation(lookDirection);
+                }
+
+            }
+
+            Vector3 charPos = character.transform.position; //current character postiioin
+            charPos.y += 8; //correct postition to be diagonally above
+            charPos.z -= 8;
+            if (cam.transform.position != charPos)
+            {
+
+                cam.transform.position = charPos;
+
+                /*Ray r = new Ray(cam.transform.position, cam.transform.forward);
+                RaycastHit hit;
+                if (Physics.Raycast(r, out hit))
+                {
+                    if (hit.collider.gameObject != gameObject)
+                    {
+                        hit.collider.gameObject.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0.5f);
+                    }
+                }*/
+            }
+            if (Input.GetKeyDown(KeyCode.F) && isInteractable)
+            {
+                Debug.Log(interactableObj.GetComponent<DialogueProperties>().dialog);
+            }
+            if (Input.GetKeyDown(KeyCode.F) && hasSetHint && doorCollider != null)
+            {
+                //fade in
+                uis.fadeOut();
+
+                //get the door properties
+                DoorProperties doorProperties = doorCollider.gameObject.GetComponent<DoorProperties>();
+                transform.position = doorProperties.teleportPosition;
+                FindObjectOfType<AudioManager>().Play("Door Enter");
+
+                //set is inside house
+                isInHouse = doorProperties.leadingInside;
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) != Vector3.zero)
+                    thrower.Throw(direction);
+                else
+                    thrower.Throw(transform.forward);
+            }
         }
     }
     void FixedUpdate()
